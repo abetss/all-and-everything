d8349b7bc163d96b7d6955e74c703257658ad72d
taras101 <taras101@gmail.com>
2019-06-10 20:14:48 -0400

    started list page

--- beelzebub-web/src/features/terms-notes/scenes/terms-list/terms-list.connector.tsx
+++ beelzebub-web/src/features/terms-notes/scenes/terms-list/terms-list.connector.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { Box, Paper, Typography } from '@material-ui/core';
+import { Box, Paper, Typography, Grid, List, ListItem, AppBar, Toolbar } from '@material-ui/core';
+import { termsMocks } from '.';
 
 export class TermsListConnector extends React.PureComponent<
 	RouteComponentProps<{}>,
 	{}
 > {
 	render() {
+		function handleClick(id: React.MouseEvent){
+			console.log(id)
+		}
+		console.log(termsMocks)
+		const tempTerms = termsMocks.map(term => <ListItem button  key={term.id}>{term.title}</ListItem>) 
 		return (
-			<Paper>
-				<Box p={2}>
-					<Typography variant="h3">Terms List</Typography>
-					<Box mt={2}>
-						<Typography variant="body1">Alkadan</Typography>
-						<Typography variant="body1">ABC</Typography>
-					</Box>
-				</Box>
-			</Paper>
+			
+			<Grid container spacing={2}  direction="column">
+				<Grid item xs={4}>
+					<Paper>
+						<AppBar position="static" color="default">
+							<Toolbar>
+							<Typography variant="h6" color="inherit">
+								Terms List
+							</Typography>
+							</Toolbar>
+						</AppBar>
+
+						<List>
+							{tempTerms}
+						</List>
+					</Paper>
+				</Grid>
+			</Grid>
+			
 		);
 	}
 }
