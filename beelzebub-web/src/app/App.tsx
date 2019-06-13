1e74408ec7ad8b85a673041bc972cf70258e88cf
Abtin Ghods <abtin.ghods@gmail.com>
2019-06-12 23:15:22 -0400

    [#166583061] sets up apollo client and hooks it to terms list

--- beelzebub-web/src/app/App.tsx
+++ beelzebub-web/src/app/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
 import { materialUiTheme } from '../design-system';
 import NoSsr from '@material-ui/core/NoSsr';
 import { Routes } from './routes';
+import { createClient } from './apollo';
+
+const client = createClient();
+
+// Using both ApolloProvider and ApolloHooksProvider in case we don't want to use
+// apollo hooks all the time. Otherwise, we can remove the ApolloProvider later
 
 export default class App extends React.PureComponent {
 	render() {
 		return (
 			<NoSsr>
 				<ThemeProvider theme={materialUiTheme}>
-					<Routes />
+					<ApolloProvider client={client}>
+						<ApolloHooksProvider client={client}>
+							<Routes />
+						</ApolloHooksProvider>
+					</ApolloProvider>
 				</ThemeProvider>
 			</NoSsr>
 		);
---
+++ beelzebub-web/src/app/apollo.tsx
@@ -0,0 +1,35 @@
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+	if (graphQLErrors) {
+		graphQLErrors.forEach(({ message, path }) =>
+			console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`)
+		);
+	}
+
+	if (networkError) {
+		console.log(
+			`[Network error ${operation.operationName}]: ${networkError.message}`
+		);
+	}
+});
+
+export const createClient = () => {
+	const cache = new InMemoryCache();
+
+	// TODO: pull this from .env
+	const httpLink = new HttpLink({
+		uri: `http://localhost:4000/`
+	});
+
+	const client = new ApolloClient({
+		cache,
+		link: ApolloLink.from([errorLink, httpLink])
+	});
+
+	return client;
+};
