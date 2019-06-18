3b5b161111790c274ccf713197be27eba80ead9c
taras101 <taras101@gmail.com>
2019-06-18 19:24:40 -0400

    notes started

---
+++ beelzebub-web/src/features/terms-notes/scenes/terms-list/notes/notes-component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Notes = (terms: any, selectedId: number) => {
+  //let selectedNote = terms.id[selectedId];
+  console.log(terms);
+  return (
+    <div>
+      <h1>Hi </h1>
+    </div>
+  );
+};
--- beelzebub-web/src/features/terms-notes/scenes/terms-list/terms-list.component.tsx
+++ beelzebub-web/src/features/terms-notes/scenes/terms-list/terms-list.component.tsx
@@ -1,43 +1,56 @@
-import React, { useState, useCallback } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { Paper, Grid, Box } from '@material-ui/core';
-import { TermsList, TermsSearch, TermsHeader } from './components';
-import { useQuery } from 'react-apollo-hooks';
+import React, { useState, useCallback } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { Paper, Grid, Box } from "@material-ui/core";
+import { TermsList, TermsSearch, TermsHeader } from "./components";
+import { Notes } from "./notes/notes-component";
+import { useQuery } from "react-apollo-hooks";
 
 // TODO: to be moved to a different file
 const findStringMatch = (input: string) => (item: any) => {
-	return item.title.toLowerCase().search(input.toLowerCase()) !== -1;
+  return item.title.toLowerCase().search(input.toLowerCase()) !== -1;
 };
 
 // TODO: to be moved to a different file
 const useTerms = (terms: any) => {
-	const [allTerms] = useState(terms);
-	const [filteredTerms, setFilteredTerms] = useState(allTerms);
+  const [allTerms] = useState(terms);
+  const [filteredTerms, setFilteredTerms] = useState(allTerms);
 
-	const handleClick = useCallback(id => {
-		console.log('id', id);
-	}, []);
+  const handleClick = useCallback(id => {
+    console.log("id", id);
+    const selectedId = id;
+  }, []);
 
-	const filterList = useCallback((input: string) => {
-		const updatedList = allTerms.filter(findStringMatch(input));
-		setFilteredTerms(updatedList);
-	}, []);
+  const filterList = useCallback((input: string) => {
+    const updatedList = allTerms.filter(findStringMatch(input));
+    setFilteredTerms(updatedList);
+  }, []);
 
-	return [allTerms, filteredTerms, handleClick, filterList];
+  return [allTerms, filteredTerms, handleClick, filterList];
 };
 
 export const TermsListComponent = ({ terms }: any) => {
-	const [allTerms, filteredTerms, handleClick, filterList] = useTerms(terms);
+  const [
+    allTerms,
+    filteredTerms,
+    handleClick,
+    filterList,
+    selectedId
+  ] = useTerms(terms);
 
-	return (
-		<Grid container justify="center" direction="column">
-			<Grid item xs={4}>
-				<Paper>
-					<TermsHeader />
-					<TermsSearch terms={allTerms} onChange={filterList} />
-					<TermsList terms={filteredTerms} onClick={handleClick} />
-				</Paper>
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid container justify="center" direction="row">
+      <Grid item xs={4}>
+        <Paper>
+          <TermsHeader />
+          <TermsSearch terms={allTerms} onChange={filterList} />
+          <TermsList terms={filteredTerms} onClick={handleClick} />
+        </Paper>
+      </Grid>
+      <Grid item xs={8}>
+        <Paper>
+          <Notes terms={allTerms} selectedId={selectedId} />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
