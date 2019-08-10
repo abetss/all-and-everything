cb5aaf7d77979630fda81385abda98ae2a22910b
taras101 <taras101@gmail.com>
2019-08-10 12:14:36 -0400

    started add pages -stuck on typescript error

--- web/src/features/terms/terms-create/terms-create-pages.component.tsx
+++ web/src/features/terms/terms-create/terms-create-pages.component.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Card } from 'rebass';
+import { string } from 'prop-types';
 
-export const TermsCreatePages = () => (
-  <form>
-    <label>
-      Pages:
-      <input type="text" name="page" />
-    </label>
-    <input type="submit" value="Submit" />
-  </form>
-);
+export const TermsCreatePages = () => {
+  let [page, setPage] = useState<string | undefined>();
+  let [pages, setPages] = useState(Array(''));
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(pages);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Pages:
+        <input
+          type="text"
+          name="page"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setPage((page = event.target.value))
+          }
+        />
+      </label>
+
+      <Button
+        onClick={() => {
+          setPages(pages.push(page));
+        }}
+      >
+        Add another page
+      </Button>
+
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
