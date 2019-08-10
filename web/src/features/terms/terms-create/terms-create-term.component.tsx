d1c9fdee04b8496b4e4df70b926ef9803bac1b1b
taras101 <taras101@gmail.com>
2019-08-10 09:43:38 -0400

    fixed using useState

--- web/src/features/terms/terms-create/terms-create-term.component.tsx
+++ web/src/features/terms/terms-create/terms-create-term.component.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'rebass';
 
-let term: any;
-
-function handleChange(newTerm: string): void {
-  term = newTerm;
-}
-function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-  e.preventDefault();
-  console.log(term);
-}
-
-export const TermsCreateTerm = () => (
-  <Card p={3}>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Term Name:
-        <input
-          type="text"
-          name="term"
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleChange(event.target.value)
-          }
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-    <h2>{term}</h2>
-  </Card>
-);
+export const TermsCreateTerm = () => {
+  let [term, setTerm] = useState('');
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(term);
+  }
+  return (
+    <Card p={3}>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Term Name:
+          <input
+            type="text"
+            name="term"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setTerm((term = event.target.value))
+            }
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <h2>{term}</h2>
+    </Card>
+  );
+};
