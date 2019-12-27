54f6320d622cf87ea7716607113ec388d9a1070d
Abtin Ghods <abtin.ghods@gmail.com>
2019-12-27 10:37:08 -0500

    makes sls work serverless

--- server/src/hello.ts
+++ server/src/hello.ts
@@ -3,6 +3,6 @@ import { APIGatewayEvent } from 'aws-lambda';
 export const handler = async (event: APIGatewayEvent): Promise<any> => {
   return {
     statusCode: 200,
-    body: 'Hello world'
+    body: 'Hello world what'
   };
 };
