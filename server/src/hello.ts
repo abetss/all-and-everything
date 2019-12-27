66e7eb58494785765a0a6a726f8e603ed0f136ed
Abtin Ghods <abtin.ghods@gmail.com>
2019-12-27 14:24:12 -0500

    fixes typescript module import issue

--- server/src/hello.ts
+++ server/src/hello.ts
@@ -1,8 +1,9 @@
+import { myName } from './server/server';
 import { APIGatewayEvent } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayEvent): Promise<any> => {
   return {
     statusCode: 200,
-    body: 'Hello world what'
+    body: `Hello world what ${myName}`
   };
 };
