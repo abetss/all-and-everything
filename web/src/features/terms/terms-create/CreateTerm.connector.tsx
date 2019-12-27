e80cca2fd067fdbd3ed94d469b1f5bdc073f8db5
Abtin Ghods <abtin.ghods@gmail.com>
2019-12-26 19:25:40 -0500

    Redirects user to term list page after user creates a term

--- web/src/features/terms/terms-create/CreateTerm.connector.tsx
+++ web/src/features/terms/terms-create/CreateTerm.connector.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 import { CreateTerm } from './CreateTerm';
 
 export const CreatePageConnector = () => {
+  const history = useHistory();
   const [pages, setPages] = useState<any>([]);
   const [term, setTerm] = useState<String>('');
 
   const handleTermSubmission = useCallback(() => {
     console.log('handleTermSubmission', { pages, term });
-  }, [pages, term]);
+    history.push('/');
+  }, [pages, term, history]);
 
   const handleAddPageClicked = useCallback(
     (page: number) => {
